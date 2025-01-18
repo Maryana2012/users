73f4299ce9ce0aba6c557bd4717a4d9967f325e0
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2025-01-18 15:38:36 +0200

    user model mongoose

--- joiSchemas/usersSchema.js
+++ joiSchemas/usersSchema.js
@@ -7,12 +7,12 @@ export const userSignupSchema =  Joi.object({
     status: Joi.string().required()
 });
 
-export const userSignin = Joi.object({
+export const userSigninSchema = Joi.object({
     name: Joi.string().required(),
     password: Joi.string().required()
 });
 
-export const userUpdateShema = Joi.object({
+export const userUpdateSchema = Joi.object({
     updateName: Joi.string(),
     updateEmail: Joi.string,
     updatePassword : Joi.string,
